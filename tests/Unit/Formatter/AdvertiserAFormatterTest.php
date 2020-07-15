79af36f100d99cad7fd018e3b5fc5d6958a9a34d
Mohamed Hafez <mohamed.hafezqo@gmail.com>
2020-07-15 16:47:25 +0200

    Updated the advertisers test cases

--- tests/Unit/Formatter/AdvertiserAFormatterTest.php
+++ tests/Unit/Formatter/AdvertiserAFormatterTest.php
@@ -6,9 +6,9 @@ use App\Model\Hotel;
 use App\Model\Room;
 use App\Service\Formatter\AdvertiserAFormatter;
 use Doctrine\Common\Collections\ArrayCollection;
-use PHPUnit\Framework\TestCase;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
-class AdvertiserAFormatterTest extends TestCase
+class AdvertiserAFormatterTest extends WebTestCase
 {
     public function testFormat()
     {
@@ -24,115 +24,12 @@ class AdvertiserAFormatterTest extends TestCase
 
     protected function getMockResponse()
     {
-        return json_decode("[
-    {
-      \"name\": \"Hotel A\",
-      \"stars\": 4,
-      \"rooms\": [
-        {
-          \"code\": \"DBL-TWN\",
-          \"net_price\": \"140.00\",
-          \"taxes\": {
-            \"amount\": \"12.00\",
-            \"currency\": \"EUR\",
-            \"type\": \"TAXESANDFEES\"
-          },
-          \"total\": \"152.00\"
-        },
-        {
-          \"code\": \"HF-BD\",
-          \"net_price\": \"133.00\",
-          \"taxes\": {
-            \"amount\": \"13.00\",
-            \"currency\": \"EUR\",
-            \"type\": \"TAXESANDFEES\"
-          },
-          \"total\": \"146.00\"
-        },
-        {
-          \"code\": \"QN-RM\",
-          \"net_price\": \"144\",
-          \"taxes\": {
-            \"amount\": \"14.00\",
-            \"currency\": \"EUR\",
-            \"type\": \"TAXESANDFEES\"
-          },
-          \"total\": \"158.00\"
-        }
-      ]
-    },
-    {
-      \"name\": \"Hotel B\",
-      \"stars\": 5,
-      \"rooms\": [
-        {
-          \"code\": \"DBL-RM\",
-          \"net_price\": \"150.00\",
-          \"taxes\": {
-            \"amount\": \"15.00\",
-            \"currency\": \"EUR\",
-            \"type\": \"TAXESANDFEES\"
-          },
-          \"total\": \"165.00\"
-        },
-        {
-          \"code\": \"HF-BOD\",
-          \"net_price\": \"130.00\",
-          \"taxes\": {
-            \"amount\": \"13.00\",
-            \"currency\": \"EUR\",
-            \"type\": \"TAXESANDFEES\"
-          },
-          \"total\": \"143.00\"
-        },
-        {
-          \"code\": \"QUN-ROM\",
-          \"net_price\": \"143.00\",
-          \"taxes\": {
-            \"amount\": \"11.50\",
-            \"currency\": \"EUR\",
-            \"type\": \"TAXESANDFEES\"
-          },
-          \"total\": \"154.50\"
-        }
-      ]
-    },
-    {
-      \"name\": \"Hotel D\",
-      \"stars\": 5,
-      \"rooms\": [
-        {
-          \"code\": \"SNGRM\",
-          \"net_price\": \"100.00\",
-          \"taxes\": {
-            \"amount\": \"15.00\",
-            \"currency\": \"EUR\",
-            \"type\": \"TAXESANDFEES\"
-          },
-          \"total\": \"115.00\"
-        },
-        {
-          \"code\": \"FU-BOD\",
-          \"net_price\": \"160.00\",
-          \"taxes\": {
-            \"amount\": \"16.00\",
-            \"currency\": \"EUR\",
-            \"type\": \"TAXESANDFEES\"
-          },
-          \"total\": \"176.00\"
-        },
-        {
-          \"code\": \"POAROM\",
-          \"net_price\": \"155.00\",
-          \"taxes\": {
-            \"amount\": \"14.00\",
-            \"currency\": \"EUR\",
-            \"type\": \"TAXESANDFEES\"
-          },
-          \"total\": \"169.00\"
-        }
-      ]
-    }
-  ]", true);
+        $kernel = static::bootKernel();
+        $config = $kernel->getContainer()->getParameter('advertisers');
+        $config = new ArrayCollection($config['advertiserA']);
+
+        $response = json_decode(file_get_contents($config->get('file')), true);
+
+        return $response['hotels'];
     }
 }
