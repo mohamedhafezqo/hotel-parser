dc981488df2655a58341ad4335d9aa56bda6e8fa
Mohamed Hafez <mohamed.hafezqo@gmail.com>
2020-07-15 15:48:12 +0200

    Added the criteria builder files

---
+++ src/Service/Criteria/Contract/FilterFactoryInterface.php
@@ -0,0 +1,1 @@
+<?phpnamespace App\Service\Criteria\Contract;/** * Interface FilterFactoryInterface * * @package App\Service\Criteria\Contract */interface FilterFactoryInterface{    /**     * @param string $filter     *     * @return mixed     */    public function create(string $filter);}
---
+++ src/Service/Criteria/Contract/FilterInterface.php
@@ -0,0 +1,1 @@
+<?phpnamespace App\Service\Criteria\Contract;use Doctrine\Common\Collections\Criteria;/** * Interface FilterInterface * * @package App\Service\Criteria\Contract */interface FilterInterface{    /**     * @param Criteria $criteria     * @param $value     * @return Criteria     */    public function apply(Criteria $criteria, $value): Criteria;}
