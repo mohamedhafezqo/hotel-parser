e625c0ee1ebfec368a8c0f04902e59a8e7172ab5
Mohamed Hafez <mohamed.hafezqo@gmail.com>
2020-07-15 15:49:36 +0200

    Added the contract folder, formatter, parser and the roomFacade class

---
+++ src/Service/Contract/RoomFacadeInterface.php
@@ -0,0 +1,1 @@
+<?phpnamespace App\Service\Contract;/** * Interface RoomFacadeInterface * * @package App\Service\Contract */interface RoomFacadeInterface{    /**     * @param array $criteria     *     * @return array     */    public function findBy(array $criteria);}
---
+++ src/Service/Formatter/Contract/FormatterInterface.php
@@ -0,0 +1,1 @@
+<?phpnamespace App\Service\Formatter\Contract;use Doctrine\Common\Collections\ArrayCollection;/** * Interface FormatterInterface * * @package App\Service\Formatter\Contract */interface FormatterInterface{    /**     * @param array $data     *     * @return ArrayCollection     */    public function format(array $data): ArrayCollection;}
