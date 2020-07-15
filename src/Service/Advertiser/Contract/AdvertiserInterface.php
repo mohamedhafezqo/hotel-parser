c1d332492fbb43efbc9f8259b35b40d5eb0ec682
Mohamed Hafez <mohamed.hafezqo@gmail.com>
2020-07-15 15:48:37 +0200

    Added advertisers folder

---
+++ src/Service/Advertiser/Contract/AdvertiserInterface.php
@@ -0,0 +1,1 @@
+<?phpnamespace App\Service\Advertiser\Contract;/** * Interface AdvertiserInterface * * @package App\Service\Advertiser\Contract */interface AdvertiserInterface{    /**     * @return array     */    public function fetch(): array;}
